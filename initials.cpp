b5b3bc921464aabadc1511906732143f4dd36928
acusticprogramming <48106782+acusticprogramming@users.noreply.github.com>
2019-02-28 22:58:54 +0100

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// Fun Fact: i dont like moth
+int main() {
+  // Your program lives here:
+  std::cout << "  A    SSS \n";
+  std::cout << " A A  S   S\n";
+  std::cout << "A   A S\n";
+  std::cout << "AAAAA  SSS\n";
+  std::cout << "A   A     S\n";
+  std::cout << "A   A S   S\n";
+  std::cout << "A   A  SSS\n";
+}
